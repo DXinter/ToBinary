05f2cd289c6277d160bf3d6e4b5bde8083e93ba6
DXinter <58430974+DXinter@users.noreply.github.com>
2019-12-09 19:22:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ToBinary
+{
+    /*
+     *функция переводящая число в двоичный код
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            ulong input = Convert.ToUInt64(Console.ReadLine());
+            string result = ToBinary(input);
+            Console.WriteLine(result);
+            Console.WriteLine("Обратный перевод: ");
+            BackIn(result);
+
+        }
+
+        private static void BackIn(string result)
+        {
+            string r = Convert.ToInt64(result, 2).ToString();
+            Console.WriteLine(r);
+        }
+
+        private static string ToBinary(ulong input)
+        {
+            char[] BinaryArray;                
+            string Result = "";
+
+            while (input > 0)
+            {
+                Result += input % 2;              
+                input = input / 2;
+            }
+
+            BinaryArray = Result.ToCharArray();
+            Array.Reverse(BinaryArray);
+            Result = new string(BinaryArray);
+            return Result;
+        }
+    }
+}
